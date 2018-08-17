6b86fe1a29fb63dd34c3c4bdc44bbdba2a87f5dd
Padikar <madhu.pad.m@hpe.com>
2018-08-17 16:22:24 +0530

    user management

--- userManagement/routes/users.js
+++ userManagement/routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mongodb = require('mongoose');
-
-var userSchema = require('../model/schema.js')
+var dbCrud = require('../service/crud');
 var router = express.Router();
 
 /* GET users listing. */
@@ -10,60 +9,40 @@ router.get('/', function(req, res, next) {
 });
 
 router.get('/signIn', function(req, res, next) {
-  mongodb.connect(url)
+
   const auth = {login: 'yourlogin', password: 'yourpassword'} // change this
 
   // parse login and password from headers
   const b64auth = (req.headers.authorization || '').split(' ')[1] || ''
   const [login, password] = new Buffer(b64auth, 'base64').toString().split(':')
   console.log("userName and pass " + login + " " + password)
-  userSchema.find({
-    'userName' : login,
-    'password' : password
-  },function(err, result) {
-    if(err) return err
-      if(result && result.length){
-        console.log(JSON.stringify(result))
-        res.send("Login Success")
-      }
 
-      else {
-        res.send("Login Failed")
-      }
-  })
-
+  dbCrud.readUser(login, password)
+      .then(function(result) {
+        console.log("Success");
+        res.status(200).json({code:200,status:'success',message:'User SignedIn Successfully'})
+      }, function(err) {
+        console.log("faild");
+        res.status(401).json({code:401,status:'failed',message:"Unauthorized"})
+      });
 });
-var url = "mongodb://localhost:27017/myDb";
+
 
 router.post('/signUp', function(req, res, next) {
 var obj = req.body;
-mongodb.connect(url)
-// var newUser = new mongodb.Schema({
-//   userName : String,
-//   password: String,
-//   address: String
-// });
-//var usermodel = mongodb.model('user', newUser)
-userSchema.find({
-  'userName' : obj.userName
-},function(err,result){
-  if(err) return err
-    if(result && result.length){
-      console.log(JSON.stringify(result))
-      res.send("User is present")
-    }
-    else {
-      var userObj = new userSchema({
-        userName : obj.userName,
-        password : obj.password,
-        address : obj.address
-      });
 
-      userObj.save()
+dbCrud.findUserName(obj.userName).then(function(result){
+    console.log("User not found")
+    dbCrud.writeLogon(obj.userName,obj.password,obj.address).then(function(result){
+      res.status(200).json({code:200,status:'success',message:'User SignedUp Successfully'})
+    }, function(err){
+      res.status(400).json({code:400,status:'failed',message:JSON.stringify(err)})
+    })
 
-      res.send("Success")
-    }
-})
+},function(err){
+  console.log("User present")
+  res.status(400).json({code:400,status:'failed',message:'User is Already Present, Please SignIn'})
+});
 });
 
 
